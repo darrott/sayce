20bf9785581afe80860eeaa9013231372d9f7faa
Dar Rott <dar.rott@gmail.com>
2023-03-29 23:12:47 +0200

    first working deploy

--- backend/index.js
+++ backend/index.js
@@ -4,177 +4,181 @@ const cors = require('cors');
 const http = require('http');
 require('dotenv').config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*'
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const server = http.createServer(app);
 const socketIO = require('socket.io');
 const io = socketIO(server, {
-	cors: {
-		origin: process.env.SOCKET_ENDPOINT,
-		methods: ['GET', 'POST']
-	}
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 });
 
 const { v4: uuidv4 } = require('uuid');
 
 const mysql = require('mysql');
 const connection = mysql.createPool({
-	connectionLimit: 50,
-	host: process.env.DBHOST,
-	port: process.env.DBPORT,
-	user: process.env.DBUSER,
-	password: process.env.DBPASS,
-	database: process.env.DBNAME
+  connectionLimit: 50,
+  host: process.env.DBHOST,
+  port: process.env.DBPORT,
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBNAME
 });
 
 io.on('connection', (socket) => {
-	console.log(`Benvenuto ${socket.id}`);
+  console.log(`Benvenuto ${socket.id}`);
 
-	const { roomId } = socket.handshake.query;
+  const { roomId } = socket.handshake.query;
 
-	socket.join(roomId);
+  socket.join(roomId);
 
-	socket.on('leave-room', (roomId) => {
-		socket.leave(roomId);
-		console.log(`${socket.id} lascia la room ${roomId}`);
-	});
+  socket.on('leave-room', (roomId) => {
+    socket.leave(roomId);
+    console.log(`${socket.id} lascia la room ${roomId}`);
+  });
 });
 
 app.get('/', async (req, res) => {
-	res.send('Attivo');
+  res.send('Attivo');
 });
 
 app.post('/table/create', async (req, res) => {
-	try {
-		connection.query('INSERT INTO tavoli (uuid) VALUE (UUID())', (error, results, fields) => {
-			if (error) {
-				res.send(error);
-				throw error;
-			}
-			connection.query(
-				`SELECT uuid FROM tavoli WHERE id = ${results.insertId}`,
-				(error, results, fields) => {
-					if (error) {
-						res.send(error);
-						throw error;
-					}
-					res.send(results[0]);
-				}
-			);
-		});
-	} catch (error) {
-		res.send(error);
-	}
+  try {
+    connection.query('INSERT INTO tavoli (uuid) VALUE (UUID())', (error, results, fields) => {
+      if (error) {
+        res.send(error);
+        throw error;
+      }
+      connection.query(
+        `SELECT uuid FROM tavoli WHERE id = ${results.insertId}`,
+        (error, results, fields) => {
+          if (error) {
+            res.send(error);
+            throw error;
+          }
+          res.send(results[0]);
+        }
+      );
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.post('/table/user/insert', (req, res) => {
-	let tableId = req.body.tableId;
-	let username = req.body.username.username;
-	let uuid = req.body.username.uuid;
-	try {
-		let query =
-			uuid != '' && uuid != 'undefined'
-				? `INSERT INTO tavolo (tavolo, username, userUUID) VALUES ('${tableId}', '${username}', '${uuid}')`
-				: `INSERT INTO tavolo (tavolo, username, userUUID) VALUES ('${tableId}', '${username}', UUID())`;
-		connection.query(query, (error, results, fields) => {
-			if (error) {
-				res.send({ state: 'Could not insert USER into DB', data: error });
-				throw error;
-			}
-			io.to(tableId).emit('nuovo-partecipante', {
-				table: tableId,
-				action: 'update-partecipanti'
-			});
-			connection.query(
-				`SELECT userUUID FROM tavolo WHERE id = ${results.insertId}`,
-				(error, results, fields) => {
-					if (error) {
-						res.send({ state: 'Could not retrieve USER UUID', data: error });
-					}
-					res.send({ state: 'success', data: results[0] });
-				}
-			);
-		});
-	} catch (error) {
-		res.send(error);
-	}
+  let tableId = req.body.tableId;
+  let username = req.body.username.username;
+  let uuid = req.body.username.uuid;
+  try {
+    let query =
+      uuid != '' && uuid != 'undefined'
+        ? `INSERT INTO tavolo (tavolo, username, userUUID) VALUES ('${tableId}', '${username}', '${uuid}')`
+        : `INSERT INTO tavolo (tavolo, username, userUUID) VALUES ('${tableId}', '${username}', UUID())`;
+    connection.query(query, (error, results, fields) => {
+      if (error) {
+        res.send({ state: 'Could not insert USER into DB', data: error });
+        throw error;
+      }
+      io.to(tableId).emit('nuovo-partecipante', {
+        table: tableId,
+        action: 'update-partecipanti'
+      });
+      connection.query(
+        `SELECT userUUID FROM tavolo WHERE id = ${results.insertId}`,
+        (error, results, fields) => {
+          if (error) {
+            res.send({ state: 'Could not retrieve USER UUID', data: error });
+          }
+          res.send({ state: 'success', data: results[0] });
+        }
+      );
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.post('/table/user/updateCart', (req, res) => {
-	let piatti = JSON.stringify(req.body.piatti);
-	let tableId = req.body.tableId;
-	let userUUID = req.body.userUUID;
-	try {
-		connection.query(
-			`UPDATE tavolo SET ordinazione = '${piatti}' WHERE userUUID = '${userUUID}' AND tavolo = '${tableId}'`,
-			(error, results, fields) => {
-				if (error) {
-					res.send({ state: 'Could not update ordinazione', data: error });
-					throw error;
-				}
-				res.send({ state: 'success' });
-			}
-		);
-	} catch (error) {
-		res.send({ state: 'Erorr' });
-	}
+  let piatti = JSON.stringify(req.body.piatti);
+  let tableId = req.body.tableId;
+  let userUUID = req.body.userUUID;
+  try {
+    connection.query(
+      `UPDATE tavolo SET ordinazione = '${piatti}' WHERE userUUID = '${userUUID}' AND tavolo = '${tableId}'`,
+      (error, results, fields) => {
+        if (error) {
+          res.send({ state: 'Could not update ordinazione', data: error });
+          throw error;
+        }
+        res.send({ state: 'success' });
+      }
+    );
+  } catch (error) {
+    res.send({ state: 'Erorr' });
+  }
 });
 
 app.post('/table/user/getCart', (req, res) => {
-	let tableId = req.body.tableId;
-	let userUUID = req.body.userUUID;
-	try {
-		connection.query(
-			`SELECT ordinazione FROM tavolo WHERE userUUID = '${userUUID}' AND tavolo = '${tableId}'`,
-			(error, results, fields) => {
-				if (error) {
-					res.send({ state: 'Could not get ordinazione', data: error });
-					throw error;
-				}
-				res.send({ state: 'success', data: results });
-			}
-		);
-	} catch (error) {
-		res.send({ state: 'Error' });
-	}
+  let tableId = req.body.tableId;
+  let userUUID = req.body.userUUID;
+  try {
+    connection.query(
+      `SELECT ordinazione FROM tavolo WHERE userUUID = '${userUUID}' AND tavolo = '${tableId}'`,
+      (error, results, fields) => {
+        if (error) {
+          res.send({ state: 'Could not get ordinazione', data: error });
+          throw error;
+        }
+        res.send({ state: 'success', data: results });
+      }
+    );
+  } catch (error) {
+    res.send({ state: 'Error' });
+  }
 });
 
 app.post('/table/getTotal', (req, res) => {
-	let tableId = req.body.tableId;
-	try {
-		connection.query(
-			`SELECT ordinazione FROM tavolo WHERE tavolo = '${tableId}' AND ordinazione != ''`,
-			(error, results, fields) => {
-				if (error) {
-					res.send({ state: 'Could not get all ordinazioni', data: error });
-					throw error;
-				}
-				res.send({ state: 'success', data: results });
-			}
-		);
-	} catch (error) {
-		res.send({ state: 'Error' });
-	}
+  let tableId = req.body.tableId;
+  try {
+    connection.query(
+      `SELECT ordinazione FROM tavolo WHERE tavolo = '${tableId}' AND ordinazione != ''`,
+      (error, results, fields) => {
+        if (error) {
+          res.send({ state: 'Could not get all ordinazioni', data: error });
+          throw error;
+        }
+        res.send({ state: 'success', data: results });
+      }
+    );
+  } catch (error) {
+    res.send({ state: 'Error' });
+  }
 });
 
 app.post('/table/seated', (req, res) => {
-	let tableId = req.body.tableId;
-	try {
-		connection.query(
-			`SELECT username FROM tavolo WHERE tavolo = '${tableId}'`,
-			(error, results, fields) => {
-				if (error) {
-					res.send({ state: 'Could not retrieve users at table', data: error });
-					throw error;
-				}
-				res.send({ state: 'sucess', data: results });
-			}
-		);
-	} catch (error) {
-		res.send({ state: 'Error' });
-	}
+  let tableId = req.body.tableId;
+  try {
+    connection.query(
+      `SELECT username FROM tavolo WHERE tavolo = '${tableId}'`,
+      (error, results, fields) => {
+        if (error) {
+          res.send({ state: 'Could not retrieve users at table', data: error });
+          throw error;
+        }
+        res.send({ state: 'sucess', data: results });
+      }
+    );
+  } catch (error) {
+    res.send({ state: 'Error' });
+  }
 });
 
 server.listen(process.env.PORT);
