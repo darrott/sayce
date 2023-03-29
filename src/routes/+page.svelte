ec7f762cd11417feeb830e06a9557cbb4a352688
Dar Rott <dar.rott@gmail.com>
2023-03-29 02:32:04 +0200

    update endpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   async function generaTavolo(){
-    const res = await fetch(`http://${process.env.PUBLIC_WEBSITE_ADDRESS}/table/create`, {
+    const res = await fetch(`http://localhost:3001/table/create`, {
       method: "POST",
     });
 
